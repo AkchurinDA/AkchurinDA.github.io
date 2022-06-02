7bfef2f02cff0edb523612b910434c478d0d04ef
Damir Akchurin <AkchurinDA@gmail.com>
2022-06-02 15:54:11 +0600

    Trying to add a blog

--- js/main.js
+++ js/main.js
@@ -5,3 +5,10 @@ var footerResize = function() {
    $('#footer').css('position', $("body").height() + $("#footer").innerHeight() > $(window).height() ? "inherit" : "fixed");
  };
  $(window).resize(footerResize).ready(footerResize);
+
+// GET DATA FROM JSON:
+ fetch("blog-posts/blog-posts.json")
+ // Get the JSON data
+ .then(response => response.json())
+ // Use (display) the JSON data
+ .then(data => console.log(data))
