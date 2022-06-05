cfc7b5dd0e0c2396c8e50d0dd806a824cfb1a321
Damir Akchurin <AkchurinDA@gmail.com>
2022-06-05 20:22:57 +0600

    Adding blog

--- js/main.js
+++ js/main.js
@@ -1,14 +1,2 @@
 // GET CURRENT YEAR:
 document.getElementById("Year").innerHTML = new Date().getFullYear();
-
-var footerResize = function() {
-   $('#footer').css('position', $("body").height() + $("#footer").innerHeight() > $(window).height() ? "inherit" : "fixed");
- };
- $(window).resize(footerResize).ready(footerResize);
-
-// GET DATA FROM JSON:
- fetch("blog-posts/blog-posts.json")
- // Get the JSON data
- .then(response => response.json())
- // Use (display) the JSON data
- .then(data => console.log(data))
